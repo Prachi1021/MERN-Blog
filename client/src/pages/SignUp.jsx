690aced8cf26c19658bfecbaea7a0e6900404585
Prachi <prachisuryawanshi21@gmail.com>
2024-05-24 21:57:19 +0530

    add functionality to the sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Button, Label, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link, useNavigate} from 'react-router-dom'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 
 export default function SignUp() {
+
+  //for seeing any change in the input section of username, email, password
+  // const handleChange = (e) =>{
+  //   console.log(e.target.value);
+  // }
+
+  const [formData, setFormData] = useState({});//empty obj
+
+  const [errorMessage, setErrorMessage] = useState(null); //any type of error
+  const [loading, setLoading] = useState(false); //loading effect due to low network connectivity
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() }); //formData will give username as well when we will chng email in the obj , id will track chng of which part is changed
+  }
+
+  // console.log(formData);
+
+  const handleSubmit = async (e) => { // async used as submitting to database takes time so we use await
+    e.preventDefault(); //avoids refreshing of page as we click on signup button
+
+    if(!formData.username || !formData.email || !formData.password){
+      return setErrorMessage('Please fill out all the fields'); 
+    }
+    
+    //for submitting data
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+
+      const res = await fetch('api/auth/signup', {         //fetch to fetch data
+        method : 'POST',
+        headers: { 'Content-Type': 'application/json' }, //get headers in the json form
+        body: JSON.stringify(formData), //to convert json into string data format
+      });
+      
+      const data = await res.json(); //convert to data
+
+      if(data.success === false){
+        return setErrorMessage(data.message);
+      }
+
+      if(res.ok){
+        navigate('/sign-in')
+      }
+
+      setLoading(false);
+
+
+    } catch (error) {
+      setErrorMessage(error.message); //client side error
+      setLoading(false);
+      
+    }
+
+  }
+
+
+
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -28,36 +88,36 @@ export default function SignUp() {
         {/* right */}
         <div className='flex-1'>
 
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
 
             <div>
               <Label value='Your username' />
-              <TextInput
-                type='text'
-                placeholder='Username'
-                id='username' //to get info for submitting the form
-              />
+              <TextInput type='text' placeholder='Username' id='username' //to get info for submitting the form 
+                onChange={handleChange} />
             </div>
 
             <div>
               <Label value='Your email' />
-              <TextInput
-                type='text'
-                placeholder='name@company.com'
-                id='email' //to get info for submitting the form
-              />
+              <TextInput type='email' placeholder='name@company.com' id='email' //to get info for submitting the form
+                onChange={handleChange} />
             </div>
 
             <div>
               <Label value='Your Password' />
-              <TextInput
-                type='text'
-                placeholder='Password'
-                id='password' //to get info for submitting the form
-              />
+              <TextInput type='password' placeholder='Password' id='password' //to get info for submitting the form
+                onChange={handleChange} />
             </div>
 
-            <Button gradientDuoTone="purpleToPink" type='submit'>Sign Up</Button>
+            <Button gradientDuoTone="purpleToPink" type='submit' disabled={loading}> 
+            {
+              loading ? (
+                <>
+                <Spinner size='sm'/>
+                <span className='pl-3'>Loading...</span> 
+                </>  //more than 1 html elements in () so add <>
+              ) : 'Sign Up'
+            }
+            </Button>
 
           </form>
 
@@ -65,6 +125,14 @@ export default function SignUp() {
             <span>Have an account?</span>
             <Link to='/sign-in' className='text-blue-500'>Sign In</Link>
           </div>
+
+          {
+            errorMessage && (
+              <Alert className='mt-5' color='failure'>
+                {errorMessage}
+              </Alert>
+            )
+          }
 
         </div>
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  //as frontend is 5173 and backend is 3000 so to convert we use proxy
+  server:{
+    proxy:{
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: 'false', //because this is http not https
+      },
+    },
+  },
   plugins: [react()],
 })
