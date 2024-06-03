06763a55afd467083f92e6ab9e6f435e77b1696e
Prachi <prachisuryawanshi21@gmail.com>
2024-06-03 22:14:59 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 //next to use middleware
 export const signup = async (req, res, next) =>{
@@ -31,7 +32,49 @@ export const signup = async (req, res, next) =>{
     }
     catch(error){
         next(error);
+    } 
+}
+
+//for signin- user will input fields and then cookies will be saved in the user's browser to authenticate next tym--
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body; //taking input 
+
+    if( !email || !password || email ==='' || password === ''){
+        next(errorHandler(400,'All fields are required'));
     }
 
-   
+    try {
+        //first we'll check the email  of the user--
+        const validUser = await User.findOne({email});    //await as result may take tym, User model is usd to find email(findOne)
+
+        if(!validUser){
+            return next(errorHandler(400, 'User not found'));
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password); //password is not hashed so bcrypt compares both passwords by converting first password to hashed then comparing
+
+        if(!validPassword){
+            return next(errorHandler(400, 'Invalid password')); //user should know what is wrong so use wrong credentials
+        }
+
+        //to authenticate the user- jwt is used--
+        const token = jwt.sign(  //whatever we're adding, should be encrypted
+            {id: validUser._id},  //to save in cookie this _id will be saved but in hashed form to latr authenticate the user
+
+            process.env.JWT_SECRET // got one time sessions
+        );
+
+        //remove password from rest to secure it
+        const { password: pass, ...rest} = validUser._doc;
+
+        //actually adding to cookie
+        res.status(200).cookie('access_token', token, {
+            httpOnly : true, //secure cookie
+        }). json(rest); //to return valid user
+
+        
+    } catch (error) { //catch uses middleware to send error
+        next(error);
+        
+    }
 }
