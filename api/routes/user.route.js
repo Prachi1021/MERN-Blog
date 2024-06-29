8c7471cda7b1202a7de72c0e46c315798d95036f
Prachi <prachisuryawanshi21@gmail.com>
2024-06-29 11:26:59 +0530

    add delete user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { test, updateUser } from "../controllers/user.controller.js";
+import { deleteUser, test, updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser); //to update or edit info in profile, userId(signin id) is taken as to which user we have to edit, for that updateUser function is imported
+router.delete('/delete/:userId', verifyToken, deleteUser);
 
 export default router;
