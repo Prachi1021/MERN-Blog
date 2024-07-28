80459873c4b496a0e6f1409e7cb3dd2b6a2d6d58
Prachi <prachisuryawanshi21@gmail.com>
2024-07-28 13:12:43 +0530

    add comments section to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { Sidebar }  from 'flowbite-react'
-import { HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi'
+import { HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi'
 import { signoutSuccess } from '../redux/user/userSlice.js';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
@@ -59,11 +59,20 @@ export default function DashSidebar() {
                 )}
 
                 {currentUser.isAdmin && (
-                    <Link to='/dashboard?tab=users'>
-                        <Sidebar.Item active={tab === 'users'} icon={HiOutlineUserGroup} as='div'>
-                        Users
-                        </Sidebar.Item>
-                    </Link>
+                    <>
+                          <Link to='/dashboard?tab=users'>
+                              <Sidebar.Item active={tab === 'users'} icon={HiOutlineUserGroup} as='div'>
+                                  Users
+                              </Sidebar.Item>
+                          </Link>
+
+                          <Link to='/dashboard?tab=comments'>
+                              <Sidebar.Item active={tab === 'comments'} icon={HiAnnotation} as='div'>
+                                 Comments
+                              </Sidebar.Item>
+                          </Link>
+                    </>
+                    
                 )}
                 
                 <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick ={handleSignout}>
