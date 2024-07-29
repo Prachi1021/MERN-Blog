3156bb5a66dff378f7de0c090dd7402117e5c99f
Prachi <prachisuryawanshi21@gmail.com>
2024-07-29 16:43:35 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from "cookie-parser";
+import path from 'path';
 
 dotenv.config();
 const app = express();
@@ -22,6 +23,8 @@ mongoose.connect(process.env.MONGO)
     console.log(err);
 });
 
+const __dirname = path.resolve();
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000!!");
 });
@@ -30,6 +33,13 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 
 //middleware.......... err-error in input, next-to go to next middleware
