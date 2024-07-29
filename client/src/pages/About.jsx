48a4a02584c22cee86f419e6c98e325c7f53faf1
Prachi <prachisuryawanshi21@gmail.com>
2024-07-29 16:31:16 +0530

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,33 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-          About
+    <div className='min-h-screen items-center justify-center flex'>
+      <div className='max-w-2xl mx-auto text-center p-3'>
+        <h1 className='my-7 font-semibold text-3xl text-center'>About Prachi's Blog</h1>
+
+        <div className='text-gray-500 flex flex-col gap-6 text-md'>
+          <p>
+            Welcome to Prachi's Blog! This blog was created by Prachi Suryawanshi as a personal project to share her thoughts and ideas with the
+            world. Prachi is a passionate developer who loves to write about
+            technology, coding, and everything in between.
+          </p>
+
+          <p>
+            On this blog, you'll find weekly articles and tutorials on topics
+            such as web development, software engineering, and programming
+            languages. Prachi is always learning and exploring new
+            technologies, so be sure to check back often for new content!
+          </p>
+
+          <p>
+            We encourage you to leave comments on our posts and engage with
+            other readers. You can like other people's comments and reply to
+            them as well. We believe that a community of learners can help
+            each other grow and improve.
+          </p>
+        </div>
+
+      </div>
     </div>
   )
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import CallToAction from '../components/CallToAction'
 
 export default function Projects() {
   return (
-    <div>
-          Projects
+    <div className='min-h-screen max-w-3xl p-3 mx-auto flex flex-col gap-6 items-center justify-center'>
+      <h1 className='text-3xl font-semibold'>Projects</h1>
+      <p className='text-md text-gray-500'>Build fun and engaging projects while learning HTML, CSS, and JavaScript!</p>
+      <CallToAction/>
     </div>
   )
 }
